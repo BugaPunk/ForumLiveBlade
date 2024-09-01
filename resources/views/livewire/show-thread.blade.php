7a62db78c8bc832e463e5281d1967b3c29ed37fe
BugaPunks <punksbuga@outlook.es>
2024-09-01 11:46:56 -0400

    Clase Completado Crear una respuesta principal
    error de alpinejs cuando no se limpia input, se comento app.js .\resources\js\app.js

--- resources/views/livewire/show-thread.blade.php
+++ resources/views/livewire/show-thread.blade.php
@@ -1,4 +1,4 @@
-<div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 flex gap-10 py-12 text-white">
+<div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
     <div class="rounded-md bg-gradient-to-r from-slate-800 to-slate-900 mb-4">
         <div class="p-4 flex gap-4">
             <div>
@@ -27,6 +27,14 @@
     </div>
 
     <!-- Respuestas -->
+
     <!-- Formulario -->
+    <form class="mb-4" wire:submit.prevent='postReply'>
+        <input 
+            type="text" 
+            placeholder="Escribe una respuesta" 
+            class="bg-slate-800 border-0 rounded-md w-full p-3 text-white/60 text-xs" 
+            wire:model.der="body"/>
+    </form>
 
 </div>
